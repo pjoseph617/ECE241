488a79d44c9a03910467fa6368713b7e4d66932c
Michael Zink <zink@ecs.umass.edu>
2018-09-20 14:13:14 -0400

    updated HW4

--- HW2/question4.py
+++ HW2/question4.py
@@ -1,8 +1,16 @@
 """
 A shelf contains some products sorted according to their width,
+e.g.     |||10, 20|||
+
 we place more already sorted products with lesser width than
-the one that are already present. e.g.|||10,20||1,2,4,7|||
-find the position of a given book (no sorting allowed and find it in O(log N))
+the one that are already present.
+e.g.     |||10,20||1,2,4,7|||
+
+Find the position of a given product (no sorting allowed and find it in O(log n))
+
+e.g.     |||10, 20||1, 2, 4, 7|||
+         position of product with width 10 is 0
+         position of product with width 7 is 5
 """
 
 
@@ -13,7 +21,7 @@ class Shelf:
         # TODO return the position of the product
         # TODO 'return -1' if the product is not in the shelf
 
-        return -2 # TODO remove this after you have filled this function
+        return -2  # TODO remove this after you have filled this function
 
     def locate_product(self, pos, shelf_contents):
         '''
@@ -25,23 +33,22 @@ class Shelf:
 
         if pos is not -1:
             print("\n########### Found product on the Shelf ###########")
-            for i, width in enumerate(reversed(shelf_contents)):
-                if i is len(shelf_contents) - 1 - pos:
-                    print("found product with width:{} ".format(width))
+
+            print("Found product with width:{} at position:{} ".format(shelf_contents[pos],pos))
+
             print("\n##################################################")
         else:
             print("\nNo such product!!")
 
 
-
 def main():
-
     shelf_contents = [10, 20, 1, 2, 3, 6, 7]
-    shelf_obj = CorrectShelf()
+    shelf_obj = Shelf()
     product_to_be_found = 1
     pos = shelf_obj.search_product(shelf_contents, 0, len(shelf_contents) - 1, product_to_be_found)
     print("pos:{}".format(pos))
     shelf_obj.locate_product(pos, shelf_contents)
+
 
 if __name__ == '__main__':
     main()
