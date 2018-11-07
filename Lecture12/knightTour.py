747ad5d67eaf1c0471cc1afe79b7cd5c8351cb4a
Michael Zink <zink@ecs.umass.edu>
2018-11-07 10:29:56 -0500

    updates for lectures 11 and 12

--- Lecture12/knightTour.py
+++ Lecture12/knightTour.py
@@ -1,6 +1,9 @@
-from Graph import Graph, Vertex
+from pythonds.graphs import Graph
+from knightGraph import knightGraph
+
 
 def knightTour(n,path,u,limit):
+        print("Current Vertex:{}".format(u))
         u.setColor('gray')
         path.append(u)
         if n < limit:
@@ -12,11 +15,21 @@ def knightTour(n,path,u,limit):
                     done = knightTour(n+1, path, nbrList[i], limit)
                 i = i + 1
             if not done:  # prepare to backtrack
-                path.pop()
+                bt_vertex = path.pop()
                 u.setColor('white')
+                print("Backtracking vertex {}".format(bt_vertex))
         else:
             done = True
+            print("Final path")
+            for vertex in path:
+                print(vertex)
+
         return done
 
+
 path = []
-knightTour(0,path,A,6)
+kG = knightGraph(5)
+knight_start_position = 15
+v=kG.getVertex(knight_start_position)
+number_of_squares_to_visit = 18
+knightTour(0, path, v, number_of_squares_to_visit)
